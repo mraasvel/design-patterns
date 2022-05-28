c64b41bf738ee6670313076ad62098d83c6c2501
mraasvel <mbraasveldt@gmail.com>
2022-05-28 20:51:57 +0200

    refactor + clever pointer in order to fix leak and singleton deletion problem

--- factory_singleton/src/main.cpp
+++ factory_singleton/src/main.cpp
@@ -1,122 +1,37 @@
-#include "FactoryService.hpp"
+#include "factory_service.hpp"
+#include "database.hpp"
+#include "user_service.hpp"
 #include <iostream>
 #include <string>
 #include <vector>
 #include <unordered_map>
 
-using std::string;
-using std::vector;
-using std::unordered_map;
-
-class FactoryService;
-
-struct User {
-	User(int id = -1, const string& username = "default")
-	: id(id), username(username) {}
-	User(int id = -1, string&& username = "default")
-	: id(id), username(username) {}
-
-	int id;
-	string username;
-};
-
-struct Database {
-	std::unordered_map<int, User> users;
-};
-
-class UserService : IService {
-public:
-	UserService(Database db)
-	: db(db) {
-		std::cout << "Making User Service" << std::endl;
-	}
-
-	~UserService() {}
-
-	IService* clone() {
-		return this;
-		// return UserService::build();
-	}
-
-	static IService* build() {
-		return new UserService { Database {} };
-	}
-
-	static UserService* get() {
-		auto factory = FactoryService::get();
-		return static_cast<UserService*>(factory->build(FactoryService::USER_SERVICE));
-	}
-
-	User find_user(int id) const {
-		auto user = db.users.find(id);
-		if (user == db.users.end()) {
-			printf("didn't find user\n");
-			throw 404;
-		}
-		return user->second;
-	}
-
-	void add_user(User user) {
-		db.users.insert(std::make_pair(user.id, std::move(user)));
-	}
-
-	void remove_user(int id) {
-		db.users.erase(id);
-	}
-
-	void print() const {
-		for (auto& it : db.users) {
-			printf("User {\n\tid: %d\n\tname: %s\n}\n", it.second.id, it.second.username.c_str());
-		}
-	}
-
-private:
-	Database db;
-};
-
-FactoryService::FactoryService() {
-	std::cout << "Initializing Factory" << std::endl;
-	services[USER_SERVICE] = UserService::build();
+void init_factory() {
+	FactoryService& factory = FactoryService::get();
+	DatabaseService::add(factory);
+	UserService::add(factory);
 }
 
-FactoryService::~FactoryService() {
-	for (auto& service: services) {
-		delete service.second;
-	}
-}
-
-IService* FactoryService::build(ServiceType type) {
-	auto service = services.find(type);
-	if (service == services.end()) {
-		std::cout << "didn't find service" << std::endl;
-		return nullptr;
-	}
-	return service->second->clone();
-}
-
-FactoryService* FactoryService::get() {
-	if (factory_service == nullptr) {
-		factory_service = new FactoryService {};
-	}
-	return factory_service;
-}
-
-FactoryService* FactoryService::factory_service = nullptr;
-
-void authenticate(int id) {
-	auto user_service = UserService::get();
-	auto user = user_service->find_user(id);
+void user_stuff() {
+	auto user_service = UserService::build(FactoryService::get());
+	user_service->add_user(User {
+		1, std::string { "nice" },
+	});
+	auto user = user_service->find_user(0);
+	user_service->print();
 }
 
 int main() {
-	auto user_service = UserService::get();
-	if (user_service == nullptr) {
-		printf("nullptr");
-		return 1;
-	}
-	user_service->add_user(User { 0, "Maarten" });
-	user_service->print();
-	// delete user_service;
-	authenticate(0);
+	init_factory();
+
+	FactoryService& factory = FactoryService::get();
+	std::cout << "building user service" << std::endl;
+	auto user_service = UserService::build(factory);
+
+	user_service->add_user(User {
+		0, std::string { "Maarten" },
+	});
+	user_stuff();
+	auto db = DatabaseService::build(factory);
 	return 0;
 }
